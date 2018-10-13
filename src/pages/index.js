40d2e5530b5cf19a942c97ae149c3c3dc7f4b7d4
sadness_ojisan <sadness.ojisan@gmail.com>
2018-10-13 18:46:29 +0900

    update: format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import {Center, Font, Blink, Marquee, Tile, GradationText, Rotator} from 'dasa'
+import {
+  Center,
+  Font,
+  Blink,
+  Marquee,
+  Tile,
+  GradationText,
+  Rotator,
+} from 'dasa'
 import GIMG from '../images/gatsby-icon.png'
 import '../globalStyles/global.css'
 import '../globalStyles/reset.css'
 
 const IndexPage = () => (
   <Tile image={GIMG} isGlobal={true}>
-  <Blink><Rotator><Font size={48}>WELCOME</Font></Rotator></Blink>
+    <Blink>
+      <Rotator>
+        <Font size={48}>WELCOME</Font>
+      </Rotator>
+    </Blink>
     <Center horizontal={true}>
-    <GradationText
-      fontSize={96}
-      colorArray={["red", "blue", "green"]}
-      isAnimate={true}
-    >
-      DASA
-    </GradationText>
+      <GradationText
+        fontSize={96}
+        colorArray={['red', 'blue', 'green']}
+        isAnimate={true}
+      >
+        DASA
+      </GradationText>
     </Center>
-      
-      
-      <Marquee>
-        <Font color='red'>Internet Explorerでは動作しません</Font>
-      </Marquee>
+
+    <Marquee>
+      <Font color="red">Internet Explorerでは動作しません</Font>
+    </Marquee>
   </Tile>
 )
 
